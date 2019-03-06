e8e80e27b470165b82c4af7fb7e88777ac500e1f
zvan92 <ytse.evan@gmail.com>
2019-03-05 20:50:24 -0500

    All room wall locations now set

--- TextAdventureIdea/MapManager.cpp
+++ TextAdventureIdea/MapManager.cpp
@@ -39,12 +39,74 @@ void MapManager::CreateRooms()
 	roomMap["F10"] = Room(1, 1, 0, 0);
 	roomMap["F11"] = Room(0, 1, 0, 0);
 	roomMap["F12"] = Room(1, 1, 0, 1);
-
+	// E ROW
+	roomMap["E1"] = Room(0, 1, 0, 0);
+	roomMap["E2"] = Room(1, 1, 0, 0);
+	roomMap["E3"] = Room(0, 1, 0, 0);
+	roomMap["E4"] = Room(1, 1, 0, 0);
+	roomMap["E5"] = Room(1, 0, 0, 0);
+	roomMap["E6"] = Room(1, 0, 0, 1);
+	roomMap["E7"] = Room(1, 0, 1, 0);
+	roomMap["E8"] = Room(1, 1, 0, 1);
+	roomMap["E9"] = Room(0, 0, 1, 1);
+	roomMap["E10"] = Room(1, 1, 1, 0);
+	roomMap["E11"] = Room(1, 1, 0, 0);
+	roomMap["E12"] = Room(1, 0, 0, 1);
+	// D ROW
+	roomMap["D1"] = Room(1, 1, 1, 0);
+	roomMap["D2"] = Room(1, 1, 0, 0);
+	roomMap["D3"] = Room(1, 0, 0, 0);
+	roomMap["D4"] = Room(1, 1, 0, 0);
+	roomMap["D5"] = Room(0, 1, 0, 1);
+	roomMap["D6"] = Room(0, 0, 1, 1);
+	roomMap["D7"] = Room(0, 1, 1, 0);
+	roomMap["D8"] = Room(1, 1, 0, 1);
+	roomMap["D9"] = Room(1, 0, 0, 0);
+	roomMap["D10"] = Room(1, 1, 1, 0);
+	roomMap["D11"] = Room(1, 0, 0, 0);
+	roomMap["D12"] = Room(0, 1, 0, 1);
+	// C ROW
+	roomMap["C1"] = Room(1, 1, 1, 0);
+	roomMap["C2"] = Room(1, 0, 0, 0);
+	roomMap["C3"] = Room(0, 0, 0, 1);
+	roomMap["C4"] = Room(1, 0, 1, 0);
+	roomMap["C5"] = Room(1, 1, 0, 0);
+	roomMap["C6"] = Room(0, 1, 0, 1);
+	roomMap["C7"] = Room(1, 0, 1, 0);
+	roomMap["C8"] = Room(0, 1, 0, 0);
+	roomMap["C9"] = Room(1, 1, 0, 0);
+	roomMap["C10"] = Room(1, 1, 0, 1);
+	roomMap["C11"] = Room(0, 0, 1, 0);
+	roomMap["C12"] = Room(1, 0, 0, 1);
+	// B ROW
+	roomMap["B2"] = Room(0, 0, 1, 1);
+	roomMap["B3"] = Room(0, 1, 1, 1);
+	roomMap["B4"] = Room(0, 0, 1, 1);
+	roomMap["B5"] = Room(1, 0, 1, 0);
+	roomMap["B6"] = Room(1, 1, 0, 0);
+	roomMap["B7"] = Room(0, 1, 0, 0);
+	roomMap["B8"] = Room(1, 0, 0, 0);
+	roomMap["B9"] = Room(1, 1, 0, 0);
+	roomMap["B10"] = Room(1, 1, 0, 1);
+	roomMap["B11"] = Room(0, 0, 1, 1);
+	roomMap["B12"] = Room(0, 1, 1, 0); // THE EXIT!
+	// A ROW
+	roomMap["A2"] = Room(0, 1, 1, 0);
+	roomMap["A3"] = Room(1, 1, 0, 0);
+	roomMap["A4"] = Room(0, 0, 0, 1);
+	roomMap["A5"] = Room(0, 1, 1, 0);
+	roomMap["A6"] = Room(1, 1, 0, 0);
+	roomMap["A7"] = Room(1, 1, 0, 1);
+	roomMap["A8"] = Room(0, 1, 1, 0);
+	roomMap["A9"] = Room(1, 1, 0, 0);
+	roomMap["A10"] = Room(1, 1, 0, 0);
+	roomMap["A11"] = Room(0, 1, 0, 0);
+	roomMap["A12"] = Room(1, 1, 0, 1);
 }
 
 void MapManager::AddItemToRoom(const char* item, const char* gridID)
 {
-
+	// attempt to add inventory item to room
 }
 
 void MapManager::DescribeRoom(const char* gridID)
